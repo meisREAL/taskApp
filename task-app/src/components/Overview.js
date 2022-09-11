504533ff640ff1a405b80f6798d8c5ae91085efc
meisReal <simas.cesnakauskas@gmail.com>
2022-09-11 16:30:33 +0300

    Overview class added wich renders task in main app component

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+
+class Overview extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <ul>
+                {this.props.tasks.map((task) => {
+                    return <li>{task.text}</li>
+                })}
+            </ul>
+        );
+    }
+}
+
+export default Overview
 
--- task-app/src/index.js
+++ task-app/src/index.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import Overview from './components/Overview';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            task: '',
+            task: {
+                text: '',
+            },
             tasks: [],
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event) {
-        this.setState({ task: event.target.value });
+        // this.setState({ task: event.target.value });
         // this.setState(this.state.tasks.concat(this.state.task))
-        // console.log(this.state.tasks)
+        this.setState({
+            task: {
+                text: event.target.value,
+            }
+        });
     }
     handleSubmit(event) {
         event.preventDefault();
-        this.setState({ tasks: this.state.tasks.concat(this.state.task) });
+        this.setState({
+            tasks: this.state.tasks.concat(this.state.task),
+            task: {
+                text: '',
+            }
+        });
         console.log(this.state.tasks)
         // event.preventDefault();
     }
     render() {
+        // const { task, tasks } = this.state
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Task:
-                        <input type="text" value={this.state.task} onChange={this.handleChange} />
+                        <input type="text" value={this.state.task.text} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
+                <Overview tasks={this.state.tasks} />
             </div>
         );
     }
